2c1bcb646d4302d81f78b1c117feaa373817227f
bryanStv <bryans.oyola@gmail.com>
2024-02-08 18:51:04 +0100

    MaquinaExpendedora2.0

--- src/maquinaExpendedora/MainMaquinaExpendedora.java
+++ src/maquinaExpendedora/MainMaquinaExpendedora.java
@@ -1,14 +1,38 @@
 package maquinaExpendedora;
 
 import javax.management.InvalidAttributeValueException;
+import java.util.Scanner;
 
 public class MainMaquinaExpendedora {
-    public static void main(String[] args) throws InvalidAttributeValueException {
-        MaquinaExpendedora m1 = new MaquinaExpendedora("01",500);
-        //m1.setPagado(620);
-        //System.out.println(m1);
+    public static void main(String[] args) {
+        Scanner tc = new Scanner(System.in);
+        MaquinaExpendedora m1 = new MaquinaExpendedora("01",500,8000);
+        System.out.println("El ticket "+m1.getTicket()+" cuesta "+m1.getPrecioTicket()/100+" € "+"("+m1.getPrecioTicket()+" centimos)");
+        int pagado;
 
-        m1.setPagado(3000);
-        System.out.println(m1);
+        try {
+            System.out.println("Cantidad Requerida: "+m1.getPrecioTicket()+" Cantidad Pagada: "+m1.getPagado()+" Cajón: "+m1.getCajon());
+            System.out.print("Introduce la cantidad(en centimos): ");
+            pagado = tc.nextInt();
+            m1.addMoney(pagado);
+            while(true) {
+                if(m1.getPagado() >= m1.getPrecioTicket()){
+                    if(m1.getPagado() == m1.getPrecioTicket()){
+                        System.out.println("Haz instroducido la cantidad exacta");
+                        System.out.println("Total a devolver: 0€");
+                        System.exit(0);
+                    }
+                    m1.setPagado(pagado);
+                    m1.devuelvo(pagado);
+                    break;
+                }
+                System.out.println("Cantidad Requerida: "+m1.getPrecioTicket()+" Cantidad Pagada: "+m1.getPagado()+" Cajón: "+m1.getCajon());
+                System.out.print("No has introducido suficiente dinero, por favor, añade más(en centimos): ");
+                m1.addMoney(tc.nextInt());
+            }
+        }catch (InvalidAttributeValueException e){
+            System.out.println("Cantidad Requerida: "+m1.getPrecioTicket()+" Cantidad Pagada: "+m1.getPagado()+" Cajón: "+m1.getCajon());
+            System.out.println("Por favor, introduzca un valor correcto");
+        }
     }
 }
--- src/maquinaExpendedora/MaquinaExpendedora.java
+++ src/maquinaExpendedora/MaquinaExpendedora.java
@@ -3,18 +3,18 @@ package maquinaExpendedora;
 import javax.management.InvalidAttributeValueException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MaquinaExpendedora {
     private String ticket;
     private int precioTicket;
     private int pagado;
-    private int devolucion;
-    private Map<Integer,Integer> ahorros;
+    private int cajon;
 
-    public MaquinaExpendedora(String ticket,int precioTicket) {
+    public MaquinaExpendedora(String ticket,int precioTicket, int cajon) {
         this.ticket = ticket;
         this.precioTicket = precioTicket;
-        this.ahorros = new HashMap<>();
+        this.cajon = cajon;
     }
 
     public String getTicket() {
@@ -44,36 +44,98 @@ public class MaquinaExpendedora {
             throw new InvalidAttributeValueException("Introduce la cantidad justa");
         }
     }
-
-    public int getDevolucion() {
-        this.devolucion = this.pagado - this.precioTicket;
-        return devolucion;
+    public int getCajon() {
+        return cajon;
     }
 
-    private StringBuilder devuelvo(int pagado) throws InvalidAttributeValueException{
-        setPagado(pagado);
+    public void setCajon(int cajon) {
+        this.cajon = cajon;
+    }
+
+    public void addMoney(int dinero){
+        this.pagado += dinero;
+    }
+
+    public void devuelvo(int pagado){
+        int[] monedas = {1000,500,200,100,50,20,10,5,2,1};
+        Map<Integer,Integer> cambios = new HashMap<>();
+        int moneda, resto, cambio;
+        int devolver = pagado - this.getPrecioTicket();
+        if(devolver > this.cajon){
+            System.out.println("Introduce el cambio exacto, intentelo de nuevo.");
+            System.exit(0);
+        }else{
+            this.cajon += this.getPrecioTicket();
+            this.cajon -= devolver;
+        }
+        int posicion = 0;
+        //Rellenar mapa
+        for(int i = 0; i < monedas.length;i++){
+            cambios.put(monedas[i],0);
+        }
+        Integer cantidad;
+        while(true){
+            moneda = monedas[posicion];
+            cambio = devolver/moneda;
+            resto = devolver%moneda;
+            cantidad = cambios.get(moneda);
+            if(resto == 0){
+                cambios.put(moneda,cambio);
+                break;
+            }
+            if(cambio/moneda >= 0 && cambio != 0){
+                cambios.put(moneda,cambio);
+            }
+            else{
+                posicion++;
+            }
+            devolver=resto;
+        }
+        //Ordenar Mapa de mayor a menor
+        Map<Integer, Integer> cambiosOrdenados = new TreeMap<>((clave1, clave2) -> clave2.compareTo(clave1));
+        cambiosOrdenados.putAll(cambios);
+        System.out.println("Total a devolver: ");
+        for(Integer key: cambiosOrdenados.keySet()){
+            Integer valor = cambios.get(key);
+            double euro = Double.valueOf(key)/100;
+            if(valor != 0){
+                System.out.println("\t\t"+euro+"€ --> "+valor);
+            }
+        }
+        System.out.println("Dinero en el cajón: "+this.cajon);
+    }
+
+    @Override
+    public String toString(){
+        return "Ticket: "+this.ticket + " Coste: "+this.precioTicket;
+    }
+
+    /*
+    public void devuelvo2(int pagado){
         int[] monedas = {1000,500,200,100,50,20,10,5,2,1};
         StringBuilder resultado = new StringBuilder();
         int posicion = 0;
         int devolver = pagado - this.precioTicket;
         int cambio;
-        int resto;
+        int resto , moneda;
         while(true){
-            cambio = devolver/monedas[posicion];
-            resto = devolver%monedas[posicion];
+            moneda = monedas[posicion];
+            cambio = devolver/moneda;
+            resto = devolver%moneda;
             if(resto == 0){
-                resultado.append(monedas[posicion]/100).append("€");
-                return resultado;
+                resultado.append(moneda/100).append("€");
+                //return resultado;
+                System.out.println(resultado);
             }
-            if(cambio/monedas[posicion] >= 0){
+            if(cambio/moneda >= 0){
                 if(cambio != 0) {
-                    resultado.append(monedas[posicion]/100).append("€, ");
+                    resultado.append(moneda/100).append("€, ");
                     devolver -= monedas[posicion];
-                    if(devolver >= monedas[posicion]){
+                    if(devolver >= moneda){
                         posicion = 0;
                     }
                 }
-                if(devolver < monedas[posicion]){
+                if(devolver < moneda){
                     posicion++;
                 }
             }else{
@@ -81,13 +143,5 @@ public class MaquinaExpendedora {
             }
         }
     }
-
-    @Override
-    public String toString(){
-        try {
-            return "Ticket: "+this.ticket + " Coste: "+this.precioTicket+" Pagado: "+this.pagado+" Devuelto: "+this.getDevolucion()+" Cambio: "+devuelvo(this.pagado);
-        } catch (InvalidAttributeValueException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    */
 }
